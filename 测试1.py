f96136f60584751841f711456e7163592643b974
hxl <2954246091@qq.com>
2023-06-07 10:03:39 +0800

    修改五子棋-落点提示-未完成

--- 测试1.py
+++ 测试1.py
@@ -1,36 +1,26 @@
-from tkinter import *
+def abc(s: str) -> str:
+    # 接收一个长度为n的字符串
+    n = len(s)
+    # 如果字符串只有1个或0个字符，其本身就是回文串
+    if n < 2:
+        return s
+    # 用0初始化一个nxn的动态数组
+    dp = [[0] * n for _ in range(n)]
+    for i in range(n):
+        dp[i][i] = 1
+    maxlen = 0
+    begin = 0
 
-class Win(Tk):
-    def __init__(self):
-        super().__init__()
-        self.canvas = Canvas(self, width=650, height=650, background='#D2BE96')  # #f5eeee
-        self.canvas.pack(side=LEFT)
-        self.__event_bind()
-        self.predict = None
-        self.x = None
+    for j in range(1, n):
+        for i in range(0, j):
+            if (s[i] == s[j]) and dp[i + 1][j - 1]:
+                dp[i][j] = 1
+                # 判断是否为最长回文字串
+                if j - i > maxlen:
+                    maxlen = j - i
+                    begin = i
 
-    # 事件绑定
-    def __event_bind(self):
-        self.canvas.bind("<Motion>", self.game_rules)
+    return s[begin:begin + maxlen]
 
-    def game_rules(self, event):
-        # j = (event.x - 45) / 40
-        # i = (event.y - 45) / 40  # 上临近i行，左临近j列，从左到右，从上到下
-        # if (event.x - 45) % 40 > 20:
-        #     j += 1
-        # if (event.y - 45) % 40 > 20:
-        #     i += 1
-        # self.predict = self.canvas.create_rectangle(i - 15, j - 15, i + 15, j + 15, dash=(3, 1),
-        #                                             outline="blue")
 
-        self.x = self.canvas.create_text(300, 300, text='x,y:{},{}'.format(event.x, event.y))
-        self.canvas.delete(self.x)
-        # self.canvas.after(2000, self.canvas.delete, self.x)
-
-        # if self.predict:  # 不断删除，不断更新
-        #     self.canvas.delete(self.predict)
-
-if __name__ == "__main__":
-    win = Win()
-    win.config(background="#D2B48C")
-    win.mainloop()
+print(abc("asdfgdfgfd"))
